0adcb6b8f34a6366e4cea6937a441b71ea3a8857
lina_gritsel <110789775+lina-gritsel@users.noreply.github.com>
2022-10-11 23:42:39 +0300

    feat: add solution

--- src/count-cats.js
+++ src/count-cats.js
@@ -1,9 +1,9 @@
-const { NotImplementedError } = require('../extensions/index.js');
+const { NotImplementedError } = require("../extensions/index.js");
 
 /**
  * Given matrix where you have to find cats by ears "^^"
  *
- * @param {Array<Array>} matrix 
+ * @param {Array<Array>} matrix
  * @return {Number} count of cats found
  *
  * @example
@@ -14,11 +14,25 @@ const { NotImplementedError } = require('../extensions/index.js');
  * ]) => 3`
  *
  */
-function countCats(/* matrix */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function countCats(matrix) {
+  let result = 0;
+  matrix.forEach((element) => {
+    element.forEach((el) => {
+      if (el === "^^") result++;
+    });
+  });
+  return result;
+  // let result = 0;
+  // for (let i = 0; i < matrix.length; i++) {
+  //   for (let j = 0; j < matrix[i].length; j++) {
+  //     if (matrix[i][j] === "^^") {
+  //       result++;
+  //     }
+  //   }
+  // }
+  // return Number(result);
 }
 
 module.exports = {
-  countCats
+  countCats,
 };
--- src/what-season.js
+++ src/what-season.js
@@ -1,21 +1,42 @@
-const { NotImplementedError } = require('../extensions/index.js');
+const { NotImplementedError } = require("../extensions/index.js");
 
 /**
  * Extract season from given date and expose the enemy scout!
- * 
+ *
  * @param {Date | FakeDate} date real or fake date
  * @returns {String} time of the year
- * 
+ *
  * @example
- * 
+ *
  * getSeason(new Date(2020, 02, 31)) => 'spring'
- * 
+ *
  */
-function getSeason(/* date */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function getSeason(date) {
+  if (!date) return "Unable to determine the time of year!";
+  if (
+    Object.prototype.toString.call(date) != "[object Date]" ||
+    typeof date == "string" ||
+    Object.getOwnPropertySymbols(date).length !== 0
+  ) {
+    throw new Error("Invalid date!");
+  }
+
+  const month = date.getUTCMonth() + 1;
+
+  if ((month > 0 && month < 3) || month === 12) {
+    return "winter";
+  }
+  if (month > 2 && month < 6) {
+    return "spring";
+  }
+  if (month > 5 && month < 9) {
+    return "summer";
+  }
+  if (month > 8 && month < 12) {
+    return "autumn";
+  }
 }
 
 module.exports = {
-  getSeason
+  getSeason,
 };
